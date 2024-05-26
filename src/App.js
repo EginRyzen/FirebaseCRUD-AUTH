b336e02e1d26e1122d28fbc1fd9d868c2f07c207
EginRyzen <muhammadaliagil@gmail.com>
2024-05-26 21:09:45 +0700

    feat: authcontext dan authreducer di folder  context

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
 
-  const currentUser = false;
+  const { currentUser } = useContext(AuthContext);
 
   const RequiredAuth = ({ children }) => {
-    return currentUser ? children : <Login />;
+    return currentUser ? children : <Navigate to="/login" />;
   };
 
   return (
